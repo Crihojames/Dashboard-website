f61e8a8ce120428203080d2231d9dc66915b668e
Crihojames <crihojames@gmail.com>
2024-03-09 22:42:47 +0100

    Add CSS topnav

--- src/assets/components/topnav/TopNav.jsx
+++ src/assets/components/topnav/TopNav.jsx
@@ -1,31 +1,39 @@
 import {NotificationsNone, Language, Settings, ChatBubble} from "@mui/icons-material"
+import './topnav.css'
+
 
 const TopNav = () => {
     return (
-        <div className='topnavContainer'>
-             <div className='topnavWrapper'>
-                <span className='logo'>AdminDash</span>
-             </div>
-             <div className='topnavIconContainer'>
-                <NotificationsNone className="topnavIcon"/>
-                <span className="topIconBadge">2</span>
-             </div>
-             <div className='topnavIconContainer'>
-                <Language className="topnavIcon"/>
-                <span className="topIconBadge">3</span>
-             </div>
-             <div className='topnavIconContainer'>
-                <Settings className="topnavIcon"/>
-                <span className="topIconBadge">2</span>
-             </div>
-             <div className='topnavIconContainer'>
-                <ChatBubble className="topnavIcon"/>
-                <span className="topIconBadge">2</span>
-             </div>
-             <img src="https://avatars.githubusercontent.com/u/101941972?v=4" alt="" />
-        </div>
+      <div className='topnavContainer'>
+            <div className="topnavWrapper">
+               <div className='topLeft'>
+                  <span className='logo'>AdminDash</span>
+               </div>
+               <div className="topRight">
+                     <div className='topnavIconContainer'>
+                     <NotificationsNone className="topnavIcon"/>
+                     <span className="topIconBadge">2</span>
+                  </div>
+                  <div className='topnavIconContainer'>
+                     <Language className="topnavIcon"/>
+                     <span className="topIconBadge">3</span>
+                  </div>
+                  <div className='topnavIconContainer'>
+                     <Settings className="topnavIcon"/>
+                     <span className="topIconBadge">2</span>
+                  </div>
+                  <div className='topnavIconContainer'>
+                     <ChatBubble className="topnavIcon"/>
+                     <span className="topIconBadge">2</span>
+                  </div>
+               
+               <img src="https://avatars.githubusercontent.com/u/101941972?v=4" alt="Profil" className="adminImg" />
+               </div>
+               
+         </div>
+      </div>
+             
     );
 };
 
 export default TopNav;
-
