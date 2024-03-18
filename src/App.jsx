71dac982e2731419842eebbca1d3938c47149985
Crihojames <crihojames@gmail.com>
2024-03-18 23:10:49 +0100

    push

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import SideNav from "./assets/components/sidenav/SideNav"
 import TopNav from "./assets/components/topnav/TopNav"
 import './App.css'
 import Home from "./assets/pages/home/Home"
+import { Diversity1 } from "@mui/icons-material"
 
 
 function App() {
