6903e2ec6f0d13e3ee2ab57d23e570da5361f8c7
Crihojames <crihojames@gmail.com>
2024-03-27 23:49:34 +0100

    implementing the routes parti 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
 
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import SideNav from "./assets/components/sidenav/SideNav"
 import TopNav from "./assets/components/topnav/TopNav"
 import './App.css'
 import Home from "./assets/pages/home/Home"
+import UserList from "./assets/pages/userlist/UserList"
 // import { Diversity1 } from "@mui/icons-material"
 
 
 function App() {
 
   return (
-      <div className="App">
+      <BrowserRouter>
         <TopNav/> 
         <div className="container">
           <SideNav/>
+          <Routes>
+            <Route path="/" element={<Home/>}></Route>
+            <Route path="/userlist" element={<UserList/>}></Route>
+          </Routes>
           <Home/>
         </div>
-      </div>
+      </BrowserRouter>
   )
 }
 
 export default App
 
-//pourquoi?
